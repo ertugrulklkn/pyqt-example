3837d35c289774ba547a2427b3c4838a1776c28d
ertugrul kalkan <ert.klkn@gmail.com>
2021-07-02 01:05:03 +0300

    ui changes

--- ui/ui_mainwindow.py
+++ ui/ui_mainwindow.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'form.ui'
+## Form generated from reading UI file 'mainwindow.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -17,33 +17,60 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 417)
+        MainWindow.resize(287, 182)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.Button1 = QPushButton(self.centralwidget)
-        self.Button1.setObjectName(u"Button1")
-        self.Button1.setGeometry(QRect(320, 150, 121, 41))
+        self.selectComPortComboBox = QComboBox(self.centralwidget)
+        self.selectComPortComboBox.setObjectName(u"selectComPortComboBox")
+        self.selectComPortComboBox.setGeometry(QRect(10, 10, 161, 21))
+        self.ComConnectButton = QPushButton(self.centralwidget)
+        self.ComConnectButton.setObjectName(u"ComConnectButton")
+        self.ComConnectButton.setGeometry(QRect(180, 10, 91, 23))
+        self.ledButtonOn = QPushButton(self.centralwidget)
+        self.ledButtonOn.setObjectName(u"ledButtonOn")
+        self.ledButtonOn.setGeometry(QRect(10, 40, 121, 21))
+        self.ledButtonOff = QPushButton(self.centralwidget)
+        self.ledButtonOff.setObjectName(u"ledButtonOff")
+        self.ledButtonOff.setGeometry(QRect(150, 40, 121, 21))
+        self.sendTimeButton = QPushButton(self.centralwidget)
+        self.sendTimeButton.setObjectName(u"sendTimeButton")
+        self.sendTimeButton.setGeometry(QRect(170, 70, 101, 23))
+        self.msInput = QLineEdit(self.centralwidget)
+        self.msInput.setObjectName(u"msInput")
+        self.msInput.setGeometry(QRect(10, 70, 151, 20))
+        self.msInput.setInputMethodHints(Qt.ImhDigitsOnly)
+        self.msInput.setMaxLength(10)
+        self.ledStatusTextLabel = QLabel(self.centralwidget)
+        self.ledStatusTextLabel.setObjectName(u"ledStatusTextLabel")
+        self.ledStatusTextLabel.setGeometry(QRect(10, 110, 131, 21))
+        self.ledStatusTextLabel.setFocusPolicy(Qt.NoFocus)
+        self.ledStatusTextLabel.setToolTipDuration(0)
+        self.timerStatusTextLabel = QLabel(self.centralwidget)
+        self.timerStatusTextLabel.setObjectName(u"timerStatusTextLabel")
+        self.timerStatusTextLabel.setGeometry(QRect(156, 110, 111, 21))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 21))
-        self.menuhey = QMenu(self.menubar)
-        self.menuhey.setObjectName(u"menuhey")
+        self.menubar.setGeometry(QRect(0, 0, 287, 21))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
-        self.menubar.addAction(self.menuhey.menuAction())
-
         self.retranslateUi(MainWindow)
 
         QMetaObject.connectSlotsByName(MainWindow)
+
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Form", None))
-        self.Button1.setText(QCoreApplication.translate("MainWindow", u"Push Me 2", None))
-        self.menuhey.setTitle(QCoreApplication.translate("MainWindow", u"hey", None))
-    # retranslateUi
+        self.selectComPortComboBox.setCurrentText("")
+        self.ComConnectButton.setText(QCoreApplication.translate("MainWindow", u"Connect", None))
+        self.ledButtonOn.setText(QCoreApplication.translate("MainWindow", u"Led On", None))
+        self.ledButtonOff.setText(QCoreApplication.translate("MainWindow", u"Led Off", None))
+        self.sendTimeButton.setText(QCoreApplication.translate("MainWindow", u"Send Time", None))
+        self.ledStatusTextLabel.setText("")
+        self.timerStatusTextLabel.setText("")
 
+    # retranslateUi
